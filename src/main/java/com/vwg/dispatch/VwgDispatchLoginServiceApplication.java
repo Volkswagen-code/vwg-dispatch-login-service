d74e6b4106f1bd587f81556cc4b6da145369faf9
Volkswagen-code <63095366+Volkswagen-code@users.noreply.github.com>
2020-04-03 14:40:19 +0530

    Initial commit - setting up dispatch login service

---
+++ src/main/java/com/vwg/dispatch/VwgDispatchLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vwg.dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwgDispatchLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VwgDispatchLoginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vwg/dispatch/VwgDispatchLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwg.dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VwgDispatchLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
